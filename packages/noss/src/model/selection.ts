f829631cdb2fd8d3c68bbeb6e9607ab2ab81e68a
Codefoxdev <admin@codefoxdev.com>
2024-10-04 20:31:02 +0200

    Added UnresolvedSelection and added more methods to Selection class

--- packages/noss/src/model/selection.ts
+++ packages/noss/src/model/selection.ts
@@ -1,7 +1,9 @@
+import type { Result } from "@noss-editor/utils";
 import type { Node } from "./node";
-import type { Position } from "./position";
+import type { PositionLike } from "./position";
+import { Position } from "./position";
+import { Ok } from "@noss-editor/utils";
 
-// TODO: Make immutable
 export class Selection {
   get isCollapsed() {
     // Check if same instance, or if it resolves to same position
@@ -27,8 +29,45 @@ export class Selection {
     return Selection.collapsed(pos || this.focus);
   }
 
-  static collapsed(pos: Position) {
-    return new Selection(pos, pos);
+  static collapsed(pos: Position): Selection;
+  static collapsed(pos: PositionLike): UnresolvedSelection;
+  static collapsed(pos: PositionLike) {
+    if (pos instanceof Position) return new Selection(pos, pos);
+    return new UnresolvedSelection(pos, pos);
+  }
+
+  static atStart(node: Node, offset?: number): Result<UnresolvedSelection, string>;
+  static atStart(node: Node, offset: number | undefined, boundary: Node): Result<Selection, string>;
+  static atStart(node: Node, offset?: number, boundary?: Node): Result<Selection | UnresolvedSelection, string> {
+    const pos = Position.offset(node, offset ?? 0);
+    if (boundary) return pos.resolve(boundary).map((p) => new Selection(p, p));
+    return Ok(new UnresolvedSelection(pos, pos));
+  }
+
+  static atEnd(node: Node, offset?: number): Result<UnresolvedSelection, string>;
+  static atEnd(node: Node, offset: number | undefined, boundary: Node): Result<Selection, string>;
+  static atEnd(node: Node, offset?: number, boundary?: Node): Result<Selection | UnresolvedSelection, string> {
+    const pos = Position.offset(node, node.content.size - (offset ?? 0));
+    if (boundary) return pos.resolve(boundary).map((p) => new Selection(p, p));
+    return Ok(new UnresolvedSelection(pos, pos));
+  }
+}
+
+export class UnresolvedSelection {
+  constructor(
+    readonly anchor: PositionLike,
+    readonly focus: PositionLike,
+  ) {}
+
+  resolve(boundary: Node): Result<Selection, string> {
+    return Position.softResolve(boundary, this.anchor)
+      .mapErr((e) => `Failed to resolve anchor position; ${e}`)
+      .try((anchor) =>
+        Position.softResolve(boundary, this.focus)
+          .mapErr((e) => `Failed to resolve focus position; ${e}`)
+          .map((focus) => new Selection(anchor, focus)),
+      )
+      .trace("UnresolvedSelection.resolve", "static");
   }
 }
 

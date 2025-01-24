f50e28a0e92c052b02ce503c02261ff36d23851c
Codefoxdev <admin@codefoxdev.com>
2025-01-24 21:10:50 +0100

    Fixed circular dependency issue with the slice class

--- packages/noss/src/model/slice.ts
+++ packages/noss/src/model/slice.ts
@@ -11,5 +11,10 @@ export class Slice {
     this.size = this.content.size - this.startDepth - this.endDepth;
   }
 
-  static readonly empty = new Slice(Fragment.empty, 0, 0);
+  private static emptySlice: Slice | undefined;
+  static get empty() {
+    if (!Slice.emptySlice) Slice.emptySlice = new Slice(Fragment.empty, 0, 0);
+
+    return Slice.emptySlice;
+  }
 }
